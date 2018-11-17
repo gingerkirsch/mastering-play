a76ba4b6b0c6536d98c2b81caca6a2b5b7f9e798
Aliaksandra Sankova <aliaksandra.sankova@pure360.com>
2018-11-17 23:02:13 +0000

    Little update

--- task-tracker/app/controllers/TaskController.scala
+++ task-tracker/app/controllers/TaskController.scala
@@ -3,14 +3,15 @@ package controllers
 import javax.inject._
 import models.Task
 import play.api.mvc._
-import play.twirl.api.Html
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class TaskController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class TaskController @Inject()(
+    cc: ControllerComponents
+) extends AbstractController(cc) {
 
   /**
    * Create an Action to render an HTML page with a welcome message.
@@ -18,20 +19,21 @@ class TaskController @Inject()(cc: ControllerComponents) extends AbstractControl
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index = Action {
+  def index = Action { implicit request =>
     Redirect(routes.TaskController.tasks)
   }
 
-  def tasks = Action {
+  def tasks = Action { implicit request =>
     Ok(views.html.index(Task.all))
   }
 
   def newTask = Action(parse.formUrlEncoded){ implicit request =>
     request.body.get("taskName").map(_.headOption.map(Task.add(_)))
     Redirect(routes.TaskController.index)
+
   }
 
-  def deleteTask(id: Int) = Action {
+  def deleteTask(id: Int) = Action { implicit request =>
     Task.delete(id)
     Ok
   }
